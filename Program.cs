e19efe7319bdabf6f62b842952ee45b27dbbc315
Sherzod <SherzodjonSharifov@gmail.com>
2021-05-16 16:19:15 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
+
+﻿/******************************
+ * This is a C# code that generates 
+ * EAN-13 type barcode and it can be
+ * used for books which will be stored
+ * at library.
+ * 
+ * Author Sherzod Sharifov
+ *****************************/
+
 using System;
+using System.IO;
 
 namespace Barcode
 {
@@ -6,8 +17,29 @@ namespace Barcode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            int numOfBar = 0;
+
+            //Get user input
+            Console.WriteLine("Hello, welcome to EAN-13 barcode generator app \nPlease enter number of barcodes that you need: ");
+            numOfBar = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Below are the barcodes which was requested:\n");
+
+            string barcode = "5901234123457";
+            int lastDigit = Convert.ToInt32(getLastDigit(barcode));
+
+
+            //Calculate number of barcodes
+            for (int i = 1; i < numOfBar + 1; i++)
+            {
+                Console.WriteLine("Hello World!");
+                Console.WriteLine(i);
+            }
+        }
+
+        public static string getLastDigit(string lastDigit)
+        {
+
+            return null;
         }
     }
 }
